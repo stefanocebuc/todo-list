8a843d4b36eb78a5c9a61e1ca427a502053a8b8f
stefanocebuc <stefano.cebuc@gmail.com>
2024-09-04 20:39:59 +0200

    stop saving
    Non salva automaticamente la lista se non ci sono elementi visibili

--- js/app.js
+++ js/app.js
@@ -1,102 +1,137 @@
-console.log('collegato!');
+console.log('collegato');
 
 let addButton = document.getElementById('add-button');
-addButton.addEventListener('click', function(){
-  addToDoItem();
+addButton.addEventListener('click', function () {
+    addToDoItem();
+});
+
+let pressBtn = document.getElementById('todo-entry-box');
+pressBtn.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        console.log('You pressed Enter');
+        e.preventDefault();
+        addToDoItem();
+    }
 });
 
 let clearButton = document.getElementById('clear-completed-button');
-clearButton.addEventListener('click', function(){
-  clearCompletedToDoItems();
+clearButton.addEventListener('click', function () {
+    clearCompletedToDoItems();
 });
 
 let emptyButton = document.getElementById('empty-button');
 emptyButton.addEventListener('click', emptyList);
 
-let saveButton = document.querySelector('#save-button');
+let saveButton = document.getElementById('save-button');
 saveButton.addEventListener('click', saveList);
 
-
-
 // variabile del selettore HTML che ho come id todo-entry-box
+
 var toDoEntryBox = document.getElementById('todo-entry-box');
 
 // variabile del selettore HTML che ha come id todo-list
+
 var toDoList = document.getElementById('todo-list');
 
-function newToDoItem(itemText, completed){
-  let toDoItem = document.createElement('li');
-  let toDoText = document.createTextNode(itemText);
+function newToDoItem(itemText, completed) {
+    let toDoItem = document.createElement('li');
+    let toDoText = document.createTextNode(itemText); // abbiamo creato il nodo dove inettiamo gli elementi della lista
 
-  toDoItem.appendChild(toDoText);
+    toDoItem.appendChild(toDoText);
 
-  if(completed){
-    toDoItem.classList.add('completed');
-  }
-  
-  toDoList.appendChild(toDoItem);
-  toDoItem.addEventListener('dblclick', toggleToDoItemState);
-}
-
-function addToDoItem(){
-  let itemText = toDoEntryBox.value;
-  newToDoItem(itemText, false);
-}
-
-function toggleToDoItemState(){
-  if(this.classList.contains('completed')){
-    this.classList.remove('completed');
-  }
-  else{
-    this.classList.add('completed');
-  }
-}
-
-function clearCompletedToDoItems(){
-  let completedItems = toDoList.getElementsByClassName('completed');
-
-  while(completedItems.length > 0){
-    completedItems.item(0).remove();
-  }
-}
-
-function emptyList(){
-  let toDoItems = toDoList.children;
-
-  while(toDoItems.length > 0){
-    toDoItems.item(0).remove();
-  }
-}
-
-
-function saveList(){
-  let toDos = [];
-
-  for(let i = 0; i < toDoList.children.length; i++){
-    let toDo = toDoList.children.item(i);
-
-    var toDoInfo = {
-      "task" : toDo.innerText,
-      "completed" : toDo.classList.contains('completed')
-    };
-
-    toDos.push(toDoInfo);
-    
-  }
-  console.log(toDos);
-
-  localStorage.setItem('toDos', JSON.stringify(toDos));
-}
-
-function loadList(){
-  if(localStorage.getItem('toDos') != null){
-    let toDos = JSON.parse(localStorage.getItem('toDos'));
-
-    for(let i = 0; i < toDos.length; i++){
-      let toDo = toDos[i];
-      newToDoItem(toDo.task, toDo.completed);
+    if (completed) {
+        toDoItem.classList.add('completed');
     }
-  }
+
+    toDoList.appendChild(toDoItem);
+    toDoItem.addEventListener('dblclick', toggleToDoItemState);
+}
+
+function addToDoItem() {
+    let itemText = toDoEntryBox.value;
+    newToDoItem(itemText, false);
+}
+
+function toggleToDoItemState() {
+    if (this.classList.contains('completed')) {
+        this.classList.remove('completed');
+    }
+    else {
+        this.classList.add('completed');
+    }
+}
+
+// button delete completed
+
+function clearCompletedToDoItems() {
+    let completedItems = toDoList.getElementsByClassName('completed');
+
+    while (completedItems.length > 0) {
+        completedItems.item(0).remove();
+    }
+}
+
+// button DELETE ALL
+
+function emptyList() {
+    let toDoItems = toDoList.children; // children ovvero i listItem
+
+    while (toDoItems.length > 0) {
+        toDoItems.item(0).remove();
+    }
+
+    // localStorage.clear(); // metodo per svuotare il local storage senza farlo manualmente
+}
+
+
+// save-list 
+
+function saveList() {
+    let toDos = [];
+
+    for (let i = 0; i < toDoList.children.length; i++) {
+        let toDo = toDoList.children.item(i);
+
+        var toDoInfo = {
+            "task": toDo.innerText,
+            "completed": toDo.classList.contains('completed')
+        };
+
+        toDos.push(toDoInfo);
+    }
+    console.log(toDos);
+
+    if (toDos.length !== 0) {
+        localStorage.setItem('toDos', JSON.stringify(toDos));
+    }
+}
+
+/* function stopGoInterval() {
+    let intervallo = setInterval(saveList, 1000);
+
+    let stopInterval = document.getElementById('todo-list').children;
+
+    if (stopInterval.length == 0) {
+        clearInterval(intervallo);
+    } else {
+        saveList();
+    }
+} */
+
+
+
+setInterval(saveList, 6000);
+
+function loadList() {
+    if (localStorage.getItem('toDos' != null)) {
+
+        let toDos = JSON.parse(localStorage.getItem('toDos'));
+
+        for (let i = 0; i < toDos.length; i++) {
+            let toDo = toDos[i];
+            newToDoItem(toDo.task, toDo.completed);
+        }
+    }
 }
 
 loadList();
